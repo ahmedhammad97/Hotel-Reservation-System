c3427d503f15221e8a2c4d0ffe706a8c1bba7dcb
Ahmed Hammad <hammad97official@gmail.com>
2018-12-11 01:09:04 +0200

    Fixed hotel approval

--- Services/pendingHotel.js
+++ Services/pendingHotel.js
@@ -26,20 +26,29 @@ module.exports = {
   approveHotel(req, res){
     //Keep the data for second insertions
     let sql = "SELECT * FROM pendingHotel WHERE name = ?";
-    dbConnection.query(sql, req.body.name, (err, res)=>{
-      if(err) {throw err; res.send("Failed to delete from pending");}
+    dbConnection.query(sql, req.body.name, (err1, res1)=>{
+      if(err1) {throw err; res.send("Failed to delete from pending");}
       else{
         //Remove from pending,
         sql = "DELETE FROM pendingHotel WHERE name = ?";
-        dbConnection.query(sql, req.body.name, (err, res)=>{
+        dbConnection.query(sql, req.body.name, (err2, res2)=>{
           if(err) {throw err; res.send("Failed to delete from pending");}
           else{
             //Add To Hotel
+            req.body.stars = res1[0].stars;
+            req.body.premium = res1[0].premium;
+            req.body.O_email = res1[0].O_email;
+            req.body.pool = res1[0].pool;
+            req.body.gym = res1[0].gym;
+            req.body.bar = res1[0].bar;
+            req.body.country = res1[0].country;
+            req.body.city = res1[0].city;
+            req.body.district = res1[0].district;
             creations.createHotel(req, res);
           }
         })
       }
-    }
+    })
   }
 
 }
