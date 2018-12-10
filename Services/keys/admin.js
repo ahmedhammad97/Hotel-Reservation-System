132d56a13ac4e9da9cd640a8ba231d044540c464
Ahmed Hammad <hammad97official@gmail.com>
2018-12-11 00:32:03 +0200

    Added pending relation and functionality

---
+++ Services/keys/admin.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //admin
+  username : "$2y$05$enUjNFXNnNgaj2MWnmD4gOqFVB4ECwS4FHWTdki2fdi11gGE9KPIK",
+  //0000
+  password : "$2y$05$CfmSd1f966cR0egFnOP/funedWKCNzmKaPiLdGbWrAxgxCMWtqQea"
+}
---
+++ Services/pendingHotel.js
@@ -0,0 +1,30 @@
+//Database module
+const dbconnection = require(__dirname + '/../Database/connection');
+
+module.exports = {
+
+  createPendingHotel(req, res){
+    let sql = "INSERT INTO Hotel (name, stars, premium, O_email, pool, gym, bar, city, country, district) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    //Create pending hotel
+    dbConnection.query(sql, [
+      req.body.name, req.body.stars, req.body.premium, req.body.O_email,
+      req.body.pool, req.body.gym, req.body.bar,
+      req.body.city, req.body.country, req.body.district
+    ], (err, res)=>{
+      if(err){
+        throw err;
+        res.send({"message": "Hotel creation failed"});
+      }
+      else{
+        res.send({"message" : "Created successfully"});
+      }
+    })
+  },
+
+  approveHotel(req, res){
+    //Remove from pending,
+    //Add To
+  }
+
+}
