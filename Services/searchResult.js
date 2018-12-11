4a70bce57e0fa9e2f2eff71aca2212157e73a433
Omer Elshrief <c.m_elshrief@hotmail.com>
2018-12-11 08:02:19 +0200

    Added searchResult.js service

---
+++ Services/searchResult.js
@@ -0,0 +1,67 @@
+//Dependencies
+const dbconnection = require(__dirname + '/../Database/connection');
+const bodyParser = require("body-parser");
+
+
+
+module.exports = {
+
+    getResults(req,res){
+        let hotelName = req.body.hotelname,
+            numberOfRooms = req.body.numberOfRooms,
+            price = req.body.price,
+            rate = req.body.rate,
+            countryLocation = req.body.countryLocation,
+            cityLocation = req.body.cityLocation,
+            districtLocation = req.body.districtLocation,
+            fromDate = req.body.fromDate,
+            toDate = req.body.toDate,
+            stars = req.body.stars,
+            roomType = req.vody.roomType;
+
+        let sql=`SELECT * FROM hotelroom r WHERE r.count >0 `;
+
+
+        if (countryLocation) {
+            sql += `INNER JOIN location on location.country = ${countryLocation}  `;
+        }
+        if (cityLocation) {
+            sql += `INNER JOIN location on location.city = ${cityLocation}  `;
+        }
+        if (districtLocation) {
+            sql += `INNER JOIN location on location.district = ${districtLocation}  `;
+        }
+        if(fromDate){
+            sql +=` INNER JOIN reservation on reservation.date_from <> ${fromDate} `
+        }
+        if (toDate) {
+            sql += ` INNER JOIN reservation on reservation.date_to <> ${toDate} `
+        }
+
+        if(hotelName){
+            sql +=` AND r.Hname = ${hotalName} `;
+        }
+        if (numberOfRooms) {
+            sql += ` AND r.rooms_count = ${numberOfRooms} `;
+        }
+        if (price) {
+            sql += ` AND r.rooms_count = ${price} `;
+        }
+        if(rate){
+            sql+= `AND r.price = ${rate} `;
+        }
+        if (roomType) {
+            sql += `AND r.roomType = ${roomType} `;
+        }
+        if (stars) {
+            sql += `AND r.star = ${stars} `;
+        }
+       
+        dbConnection.query(sql, reqEmail, (err, result) => {
+            if(err) throw err;
+            console.log(result);
+            return result;
+        }
+                  
+    }
+}
