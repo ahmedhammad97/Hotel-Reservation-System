1b966cb377a80bee2f553eb62f86fd7417fb16b1
Omer Elshrief <c.m_elshrief@hotmail.com>
2018-12-12 12:59:42 +0200

    SQL Edit

--- Services/searchResult.js
+++ Services/searchResult.js
@@ -26,14 +26,14 @@ module.exports = {
                     INNER JOIN reservation on reservation.roomNo = hotelroom.roomNo
                     WHERE  1=1                                                                       AND
                     ("${Hname}" ="" OR hotelroom.Hname = '${Hname}')                             AND
-                    ("${price}"=""  OR hotelroom.price = ${price} )                        AND
+                    (${price} IS NULL  OR hotelroom.price = ${price} )                        AND
                     ("${type}"=""  OR hotelroom.type = '${type}' )                         AND
                     ("${date_from}"=""   OR reservation.date_to < '${date_from}' )         AND
                     ("${date_to}"=""   OR reservation.date_from > '${date_to}' )           AND
-                    ("${bar}"=""   OR h_facilites.bar = ${bar} )                           AND
-                    ("${gym}"=""   OR h_facilites.gym = ${gym} )                           AND
-                    ("${pool}"=""   OR h_facilites.pool = ${pool})                         AND
-                    ("${city}"=""   OR location.city = '${city}')                          AND
+                    (${bar} IS NULL   OR h_facilites.bar = ${bar} )                           AND
+                    (${gym} IS NULL   OR h_facilites.gym = ${gym} )                           AND
+                    (${pool} IS NULL   OR h_facilites.pool = ${pool})                         AND
+                    (${city} IS NULL   OR location.city = '${city}')                          AND
                     ("${country}"=""   OR location.country = '${country}')                 AND
                     ("${district}"=""   OR location.district = '${district}') 
                     
