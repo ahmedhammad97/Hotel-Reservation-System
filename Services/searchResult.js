81c86e464ac3e090766204a4b97d7018f5f7555d
Omer Elshrief <c.m_elshrief@hotmail.com>
2018-12-12 13:50:04 +0200

    SQL finalizing

--- Services/searchResult.js
+++ Services/searchResult.js
@@ -20,66 +20,33 @@ module.exports = {
             gym = req.body.gym;
 
         console.log(`${req.body.Hname}`);
-        let sql =  ` SELECT * FROM hotelroom 
+
+        
+           
+        
+                    let sql =  ` SELECT * FROM hotelroom 
                     INNER JOIN h_facilites on h_facilites.Hname = hotelroom.Hname
                     INNER JOIN location on location.Hname = hotelroom.Hname
                     INNER JOIN reservation on reservation.roomNo = hotelroom.roomNo
-                    WHERE  1=1                                                                       AND
-                    ("${Hname}" ="" OR hotelroom.Hname = '${Hname}')                             AND
-                    (${price} IS NULL  OR hotelroom.price = ${price} )                        AND
+                    WHERE  1=1                                                             AND
+                    ("${Hname}" ="" OR hotelroom.Hname = '${Hname}')                       AND
+                    (${price} IS NULL  OR hotelroom.price = ${price} )                     AND
                     ("${type}"=""  OR hotelroom.type = '${type}' )                         AND
                     ("${date_from}"=""   OR reservation.date_to < '${date_from}' )         AND
                     ("${date_to}"=""   OR reservation.date_from > '${date_to}' )           AND
-                    (${bar} IS NULL   OR h_facilites.bar = ${bar} )                           AND
-                    (${gym} IS NULL   OR h_facilites.gym = ${gym} )                           AND
-                    (${pool} IS NULL   OR h_facilites.pool = ${pool})                         AND
-                    (${city} IS NULL   OR location.city = '${city}')                          AND
+                    (${bar} IS NULL   OR h_facilites.bar = ${bar} )                        AND
+                    (${gym} IS NULL   OR h_facilites.gym = ${gym} )                        AND
+                    (${pool} IS NULL   OR h_facilites.pool = ${pool})                      AND
+                    ("${city}" =""   OR location.city = '${city}')                         AND
                     ("${country}"=""   OR location.country = '${country}')                 AND
                     ("${district}"=""   OR location.district = '${district}') 
                     
                     `;
 
-                    console.log(sql);
+        console.log(sql);
+        console.log(date_from = "");
 
-
-        /*
-        if (countryLocation) {
-            sql += `INNER JOIN location on location.country = '${countryLocation} '  `;
-        }
-        if (cityLocation) {
-            sql += `INNER JOIN location on location.city = '${cityLocation}'  `;
-        }
-        if (districtLocation) {
-            sql += `INNER JOIN location on location.district = ${districtLocation}  `;
-        }
-        if(fromDate){
-            sql +=` INNER JOIN reservation on reservation.date_from <> ${fromDate} `
-        }
-        if (toDate) {
-            sql += ` INNER JOIN reservation on reservation.date_to <> ${toDate} `
-        }
-
-        if(hotelName){
-            sql +=` AND r.Hname = ${hotalName} `;
-        }
-        if (numberOfRooms) {
-            sql += ` AND r.rooms_count = ${numberOfRooms} `;
-        }
-        if (price) {
-            sql += ` AND r.rooms_count = ${price} `;
-        }
-        if(rate){
-            sql+= `AND r.price = ${rate} `;
-        }
-        if (roomType) {
-            sql += `AND r.roomType = ${roomType} `;
-        }
-        if (stars) {
-            sql += `AND r.star = ${stars} `;
-        }
-        */
-
-        
+       
 
         dbconnection.query(sql, (err, result) => {
             if(err) throw err;
