6821d4108d1f3573608a07abfef72e719bf553da
Ahmed Hammad <hammad97official@gmail.com>
2018-12-15 15:15:27 +0200

    Added some update functionalities

--- Routes/REST.js
+++ Routes/REST.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const bodyParser = require('body-parser');
 const creation = require(__dirname + '/../Services/creations');
 const auth = require(__dirname + '/../Services/auth');
+const update = require(__dirname + '/../Services/updates');
 const searchService = require(__dirname + '/../Services/searchResult');
 
 //BodyParser
@@ -18,15 +19,6 @@ router.post('/create', jsonParser, creation.createRoom);
 router.post('/approve', jsonParser, creation.approveHotel);
 router.post('/reject', jsonParser, creation.rejectHotel);
 router.post('/search',jsonParser,searchService.getResults);
-
-router.post('/fakeTime', jsonParser, (req, res)=>{
-  timer.fakeDate(req.body.time);
-  res.end()
-  //res.send("Time now: " + timer.getTimeNow())
-})
-
-router.post('/getTime', jsonParser, (req, res)=>{
-  res.send("Time now: " + timer.getTimeNow())
-})
+router.post('/rate', jsonParser, update.rateHotel)
 
 module.exports = router;
---
+++ Services/updates.js
@@ -0,0 +1,44 @@
+//Database module
+const dbConnection = require(__dirname + '/../Database/connection');
+
+module.exports = {
+
+  rateHotel(req, res){
+    // Fetch for the rating and rating count
+    let sql = "SELECT rating, r_count from Hotel WHERE name = ?";
+    dbConnection.query(sql, req.body.name, (err1, result1)=>{
+      if(err1){throw err1; res.send("Failed to update");}
+      else{
+        // Calculate new rating
+        let rating = result1[0].rating;
+        let ratingCount = result1[0].r_count;
+        rating = ((rating * ratingCount) + req.body.rating) / (ratingCount+1);
+
+        // Update the relation
+        sql = "UPDATE Hotel SET rating = ? , r_count = ? WHERE name = ?";
+        dbConnection.query(sql, [rating, ratingCount+1, req.body.name], (err2, result2)=>{
+          if(err2){throw err2; res.send("Failed to update");}
+          else{res.send({"message": "Rated successfully"});}
+        })
+      }
+    })
+  },
+
+  customerShow(req, res){
+    let sql = "UPDATE Reservation SET did_show = ? WHERE Hname = ? AND roomNo = ? AND c_email = ?";
+    dbConnection.query(sql, [req.body.show, req.body.name, req.body.room, req.body.email], (err, result)=>{
+      if(err){throw err; res.send("Failed to update");}
+      else{
+        if(req.body.show){res.send({"message": "Customer updated successfully"});}
+        else{
+          //Blacklist user
+          sql = "UPDATE Customer SET blackListed = ? WHERE email = ?";
+          dbConnection.query(sql, [true, req.body.email], (err1, result1)=>{
+            res.send({"message": "Customer black-listed successfully"});
+          })
+        }
+      }
+    })
+  }
+
+}
