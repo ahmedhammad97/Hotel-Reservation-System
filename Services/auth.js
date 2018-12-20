1509aa38350ac243baa5be0a28b1fd52e22c47a8
Omer Elshrief <c.m_elshrief@hotmail.com>
2018-12-20 23:31:11 +0200

    Customer Login Verification with cookies Customer Login redirection

--- Services/auth.js
+++ Services/auth.js
@@ -10,124 +10,146 @@ var cookieParser = require('cookie-parser');
 var sess;
 module.exports = {
 
-  adminLogin(req, res){
-    bcrypt.compare(req.body.username, keys.username, (err1, rep1)=>{
-      if(err1){throw err;}
-      if(rep1){
-        bcrypt.compare(req.body.password, keys.password, (err2, rep2)=>{
-          if(err2) throw err;
-          if(rep2){
-            res.render("brokerView", {"date": timer.getTimeNow()});
-          }
-          else{ res.send({"message": "Wrong password"});}
+    adminLogin(req, res) {
+        bcrypt.compare(req.body.username, keys.username, (err1, rep1) => {
+            if (err1) {
+                throw err;
+            }
+            if (rep1) {
+                bcrypt.compare(req.body.password, keys.password, (err2, rep2) => {
+                    if (err2) throw err;
+                    if (rep2) {
+                        res.render("brokerView", {
+                            "date": timer.getTimeNow()
+                        });
+                    } else {
+                        res.send({
+                            "message": "Wrong password"
+                        });
+                    }
+                })
+            } else {
+                res.send({
+                    "message": "Wrong username"
+                });
+            }
         })
-      }
-      else{res.send({"message": "Wrong username"});}
-    })
 
-  },
+    },
 
-  customerLogin(req, res){
-    let reqEmail, reqPassword;
-    let sess = req.session;
-    if (sess.email) {
-      reqEmail = sess.email;
-      reqPassword = sess.password;
-      console.log(`using coockies Email: ${reqEmail} PW: ${reqPassword}`);
-    } else {
-     reqEmail = req.body.email;
-     reqPassword = req.body.password;
+    customerLogin(req, res) {
+
+        reqEmail = req.body.email;
+        reqPassword = req.body.password;
+
+        //Query
+        let sql = "SELECT name, password FROM Customer WHERE email = ?";
+        dbConnection.query(sql, reqEmail, (err, result) => {
+            if (err) throw err;
+
+            //Check if any results returned
+            if (result.length != 0) {
+
+                //Verify password
+                bcrypt.compare(reqPassword, result[0].password, (err, rep) => {
+                    if (rep) {
+                        //Store user login info in cookies
+                        let sess = req.session;
+                        sess.email = req.body.email;
+                        sess.password = req.body.password;
+                        sess.resave = true;
+                        console.log(`logged in`);
+                        let redirect = req.session.redirectTo || '/';
+                        delete req.session.redirectTo;
+                        res.redirect('/');
+                    } else {
+                        res.send({
+                            "message": "Wrong password"
+                        });
+                    }
+                });
+            } else { //No matched email
+                res.send({
+                    "message": "No such a user"
+                });
+            }
+
+
+        });
+    },
+
+    customerRegister(req, res) {
+        let password = bcrypt.hashSync(req.body.password, 7);
+        let sql = `INSERT INTO Customer (email, name, password) VALUES (?, ?, ?)`;
+
+        //Query
+        dbConnection.query(sql, [req.body.email, req.body.name, password], (err, result) => {
+            if (err) {
+                throw err;
+                res.send({
+                    "message": "User already exists"
+                });
+            } else { //Valid regiseration
+                res.send(`Thank you, Fuck off`);
+                //  res.render('customerView', {"name" : req.body.name}); not done yet the Customer view
+            }
+        })
+    },
+
+    ownerLogin(req, res) {
+        let reqEmail = req.body.email;
+        //Query
+        let sql = "SELECT name, password FROM HotelOwner WHERE email = ?";
+        dbConnection.query(sql, reqEmail, (err, result) => {
+            if (err) throw err;
+
+            //Check if any results returned
+            if (result.length != 0) {
+
+                //Verify password
+                bcrypt.compare(req.body.password, result[0].password, (err, rep) => {
+                    if (rep) {
+                        res.render('ownerView', {
+                            "name": result[0].name,
+                            "email": reqEmail
+                        });
+                    } else {
+                        res.send({
+                            "message": "Wrong password"
+                        });
+                    }
+                })
+
+            } else { //No matched email
+                res.send({
+                    "message": "No such hotel owner"
+                });
+            }
+        })
+    },
+
+    ownerRegister(req, res) {
+        bcrypt.hash(req.body.password, bcryptKey.salt, (error, hash) => {
+            if (error) throw error;
+
+            let sql = `INSERT INTO HotelOwner (email, name, password) VALUES (?, ?, ?)`;
+
+            //Query
+            dbConnection.query(sql, [req.body.email, req.body.name, hash], (err, result) => {
+                if (err) {
+                    throw err;
+                    res.send({
+                        "message": "Owner already exists"
+                    });
+                } else { //Valid regiseration
+
+                    res.render('ownerView', {
+                        "name": req.body.name,
+                        "email": req.body.email
+                    });
+                }
+            })
+        });
     }
-    //Query
-    let sql = "SELECT name, password FROM Customer WHERE email = ?";
-    dbConnection.query(sql, reqEmail, (err, result)=>{
-      if(err) throw err;
-
-      //Check if any results returned
-      if(result.length != 0){
-
-        //Verify password
-        bcrypt.compare(reqPassword, result[0].password, (err, rep) => {
-          if(rep){
-            sess.email = req.body.email;
-            sess.password = req.body.password;
-            sess.resave = true;
-           // sess.cookie.path = "/";
-            //sess.cookie.secure = true;
-            //res.wrte(`Welcom ${result[0].name}`);
-            // res.render('customerView', {"name" : result[0].name});
-           // res.cookie("hello", rep);
-            res.send(`Your email ${req.body.email}`);
-          }
-          else{res.send({"message" : "Wrong password"});}
-        })
-
-      }
-      else{ //No matched email
-        res.send({"message" : "No such a user"});
-      }
-    })
-
-  },
-
-  customerRegister(req, res){
-    let password = bcrypt.hashSync(req.body.password, 7);
-    let sql = `INSERT INTO Customer (email, name, password) VALUES (?, ?, ?)`;
-
-    //Query
-    dbConnection.query(sql, [req.body.email, req.body.name, password], (err, result)=>{
-      if(err){
-          throw err;
-          res.send({"message" : "User already exists"});
-      }
-      else{ //Valid regiseration
-        res.send(`Thank you, Fuck off`);
-        //  res.render('customerView', {"name" : req.body.name}); not done yet the Customer view
-      }
-    })
-  },
-
-  ownerLogin(req, res){
-    let reqEmail = req.body.email;
-    //Query
-    let sql = "SELECT name, password FROM HotelOwner WHERE email = ?";
-    dbConnection.query(sql, reqEmail, (err, result)=>{
-      if(err) throw err;
-
-      //Check if any results returned
-      if(result.length != 0){
-
-        //Verify password
-        bcrypt.compare(req.body.password, result[0].password, (err, rep)=>{
-          if(rep){res.render('ownerView', {"name": result[0].name, "email": reqEmail});}
-          else{res.send({"message" : "Wrong password"});}
-        })
-
-      }
-      else{ //No matched email
-        res.send({"message" : "No such hotel owner"});
-      }
-    })
-  },
-
-  ownerRegister(req, res){
-    bcrypt.hash(req.body.password, bcryptKey.salt, (error, hash)=>{
-      if(error) throw error;
-
-      let sql = `INSERT INTO HotelOwner (email, name, password) VALUES (?, ?, ?)`;
-
-      //Query
-      dbConnection.query(sql, [req.body.email, req.body.name, hash], (err, result)=>{
-        if(err){
-            throw err;
-            res.send({"message" : "Owner already exists"});
-        }
-        else{ //Valid regiseration
-
-          res.render('ownerView', {"name": req.body.name, "email": req.body.email});
-        }
-      })
-    });
-  }
 
 }
